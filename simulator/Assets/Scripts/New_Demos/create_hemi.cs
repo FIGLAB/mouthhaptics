3e946a7bf4bcb2f9ca352514677f8d018852aed7
vhshen <vivianhshen@gmail.com>
2023-05-05 14:52:34 -0400

    added scripts

---
+++ simulator/Assets/Scripts/New_Demos/create_hemi.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class create_hemi : MonoBehaviour
+{
+    GameObject[] transducers;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("STARTED");
+        transducers = GameObject.FindGameObjectsWithTag("transducer");
+        Debug.Log("transducer count: " + transducers.Length.ToString());
+        foreach(GameObject trans in transducers)
+        {
+            trans.transform.LookAt(this.transform.position);
+            trans.transform.position -= transform.forward * (0.1f - Vector3.Distance(this.transform.position, trans.transform.position)); 
+            // -= new Vector3(0.0f, 0.0f, 0.01f - trans.transform.position.z);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
